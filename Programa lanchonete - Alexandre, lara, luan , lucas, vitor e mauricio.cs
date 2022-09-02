d5ff9418b0ef66426c3bce51c7e299c7b904c082
yuzo7 <112702463+yuzo7@users.noreply.github.com>
2022-09-02 09:17:18 -0300

    Add files via upload

---
+++ Programa lanchonete - Alexandre, lara, luan , lucas, vitor e mauricio.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lanches_burlis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao, opcaolanches, opcaobebidas, opcaocombos;
+
+            Double subtotal = 0;
+
+            do
+
+            {
+                Console.Clear();
+
+                Console.WriteLine("lanches burlis");
+
+                Console.WriteLine("--------------");
+
+                Console.WriteLine("");
+
+                Console.WriteLine("1- lanches e porções");
+
+                Console.WriteLine("2- bebidas");
+
+                Console.WriteLine("3- combos");
+
+                Console.WriteLine("4- finalizar");
+
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                if (opcao == 1)
+
+                {
+                    do
+
+                    {
+
+                        Console.Clear();
+
+                        Console.WriteLine("novo menu");
+
+                        Console.WriteLine("------Produto -----      -------Preço");
+
+                        Console.WriteLine("1- hamburguer simples ---------- 5.00");
+
+                        Console.WriteLine("2- Hamburguer duplo ----------- 7,50 ");
+
+                        Console.WriteLine("3- Hamburguer triplo---------- 10,00 ");
+
+                        Console.WriteLine("4- vegano -------------------- 12,00 ");
+
+                        Console.WriteLine("5- duplo com bacon ----------- 12,00 ");
+
+                        Console.WriteLine("6- duplo com cheddar --------- 14,00 ");
+
+                        Console.WriteLine("7- Triplo com bacon ---------- 15,00 ");
+
+                        Console.WriteLine("8- Triplo com cheddar -------- 16,00 ");
+
+                        Console.WriteLine("9- porção de batata ---------- 20,00 ");
+
+                        Console.WriteLine("0- Sair ---------------------------- ");
+
+                        opcaolanches = Convert.ToInt32(Console.ReadLine());
+
+                        if (opcaolanches == 1)
+
+                        {
+                            Console.Clear();
+
+                            subtotal = subtotal + 5.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " Simples seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+                        }
+
+                        if (opcaolanches == 2)
+
+                        {
+                            Console.Clear();
+
+                            subtotal = subtotal + 7.50;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " duplo seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaolanches == 3)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 10.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " triplo seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaolanches == 4)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 12.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " vegano seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaolanches == 5)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 12.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " duplo com bacon seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaolanches == 6)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 14.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " duplo com cheddar seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                            {
+
+                                if (opcaolanches == 7)
+
+                        }
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 15.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " triplo com bacon seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaolanches == 8)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 16.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + " triplo com cheddar seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaolanches == 9)
+
+                        {
+                            Console.Clear();
+
+                            subtotal = subtotal + 20.00;
+
+                            Console.WriteLine("Você escolheu o hamburguer" + "  porção de batata seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                    } while (opcaolanches != 0);
+
+                    if (opcao == 2)
+
+                    {
+
+                        do
+                    }
+                    Console.Clear();
+
+                    Console.WriteLine("novo menu");
+
+                    Console.WriteLine("------Produto -----      -------Preço");
+
+                    Console.WriteLine("1- Refrigerante Lata ---------- 4.00");
+
+                    Console.WriteLine("2- Refrigerante 600ml -------- 7,50 ");
+
+                    Console.WriteLine("3- Refrigerante 2l----------- 10,00 ");
+
+                    Console.WriteLine("0- Sair --------------------------- ");
+
+                    opcaobebidas = Convert.ToInt32(Console.ReadLine());
+
+                    if (opcaobebidas == 1)
+
+                    {
+
+                        Console.Clear();
+
+                        subtotal = subtotal + 4.00;
+
+                        Console.WriteLine("Você escolheu o refrigerante" + "  lata seu subtotal foi de:" + subtotal);
+
+                        Console.ReadKey();
+
+                    }
+
+                    if (opcaobebidas == 2)
+
+                    {
+
+                        Console.Clear();
+
+                        subtotal = subtotal + 4.00;
+
+                        Console.WriteLine("Você escolheu o refrigerante" + "  lata seu subtotal foi de:" + subtotal);
+
+                        Console.ReadKey();
+
+                    }
+
+                    if (opcaobebidas == 3)
+
+                    {
+
+                        Console.Clear();
+
+                        subtotal = subtotal + 4.00;
+
+                        Console.WriteLine("Você escolheu o refrigerante" + "  600ml seu subtotal foi de:" + subtotal);
+
+                        Console.ReadKey();
+
+                    }
+
+                    if (opcaobebidas == 4)
+
+                    {
+
+                        Console.Clear();
+
+                        subtotal = subtotal + 4.00;
+
+                        Console.WriteLine("Você escolheu o refrigerante" + "  2l seu subtotal foi de:" + subtotal);
+
+                        Console.ReadKey();
+
+                    }
+
+                } while (opcaobebidas != 0) ;
+
+                if (opcao == 3)
+
+                {
+                    do
+                    {
+
+                        Console.Clear();
+
+                        Console.WriteLine("novo menu");
+
+                        Console.WriteLine("------Produto -----      -----------Preço");
+
+                        Console.WriteLine("1 - Combo Infantil------------------ 10,00");
+
+                        Console.WriteLine("2 - Combo Duplo-------------------- 15,00 ");
+
+                        Console.WriteLine("3 - Combo Duplo Cheddar/Bacon------ 22,00 ");
+
+                        Console.WriteLine("4 - Combo Triplo Cheddar/Bacon----- 28,00 ");
+
+                        Console.WriteLine("0 - Sair -------------------------------- ");
+
+                        opcaocombos = Convert.ToInt32(Console.ReadLine());
+
+                        if (opcaobebidas == 1)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 10.00;
+
+                            Console.WriteLine("Você escolheu o combo" + "  infantil seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaobebidas == 2)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 15.00;
+
+                            Console.WriteLine("Você escolheu o combo" + "  duplo seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+                        if (opcaobebidas == 3)
+
+                        {
+
+                            Console.Clear();
+
+                            subtotal = subtotal + 28.00;
+
+                            Console.WriteLine("Você escolheu o combo" + "  duplo cheddar/bacon seu subtotal foi de:" + subtotal);
+
+                            Console.ReadKey();
+
+                        }
+
+
+                    } while (opcaocombos == 0);
+
+                    {
+                    }
+                }
+            }; } } };)
+
+            
+
+                
+
+            
+            
